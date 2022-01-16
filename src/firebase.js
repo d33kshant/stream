f72ef08c47aa01e72136fca3c56fc99a5babb1a0
Deekshant Yadav <d33kshant@gmail.com>
2022-01-16 20:16:53 +0530

    Removed login function

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,12 @@
 import { initializeApp } from "firebase/app"
-import { getAuth, GoogleAuthProvider, signInWithRedirect, signOut } from "firebase/auth"
+import { getAuth, GoogleAuthProvider, signOut } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 import config from './firebase.config'
 
 const firebaseApp =  initializeApp(config)
 
 export const auth = getAuth(firebaseApp)
-const provider = new GoogleAuthProvider()
-
-export const loginWithGoogle = () => {
-	signInWithRedirect(auth, provider)
-	.catch(alert)
-}
+export const provider = new GoogleAuthProvider()
 
 export const logout = () => signOut(auth).then(res=>window.location = '/').catch(alert)
 
