2571abf039ec027b8623c3c607fbdb40753a932d
Deekshant Yadav <d33kshant@gmail.com>
2022-01-16 17:39:43 +0530

    Fixed quotes

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './styles/index.css'
 
-ReactDOM.render( <App />, document.getElementById('root'))
+ReactDOM.render( <App />, document.getElementById("root"))
