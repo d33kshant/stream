8db30f9f7270810785cebaabbb335c2f2b40316b
Deekshant Yadav <d33kshant@gmail.com>
2022-04-11 23:12:38 +0530

    Add route for video controller

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,17 @@ require('dotenv').config()
 const expree = require('express')
 const fileUpload = require('express-fileupload')
 
+const videoRouter = require('./routes/video.route')
+
 const PORT = process.env.PORT || 5000
 
 const app = expree()
 app.use(expree.json())
-app.use(fileUpload())
+app.use(fileUpload({
+	createParentPath: true,
+}))
+
+app.use('/video', videoRouter)
 
 app.get('/', (req, res) => {
 	res.json({
