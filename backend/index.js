ad5cc2273ce364896f0576f3ba38c14b5751a9b9
Deekshant Yadav <d33kshant@gmail.com>
2022-04-24 15:43:27 +0530

    Server static dir

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 5000
 const app = express()
 app.use(express.json())
 
+app.use('/', express.static('public'))
+
 app.use('/video', videoRoute)
 
 const server = app.listen(PORT, ()=>{
