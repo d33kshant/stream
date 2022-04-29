1ee724ea8cf5ec4bb50fcae83e362b6cbc57a2ad
Deekshant Yadav <d33kshant@gmail.com>
2022-04-29 17:54:07 +0530

    Add new route scheme

--- routes/video.route.js
+++ routes/video.route.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const { streamVideo, uploadVideo } = require('../controllers/video.controller')
 
-router.get('/stream', streamVideo)
+router.get('/', streamVideo)
 router.post('/', uploadVideo)
 
 module.exports = router
