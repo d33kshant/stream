ccf395867f5e9913a7293cdfdb6aaeafd69911e4
Deekshant Yadav <d33kshant@gmail.com>
2022-05-01 11:55:09 +0530

    Add routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,20 @@
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route
+} from 'react-router-dom'
+import HomePage from '../pages/HomePage'
+import VideoPage from '../pages/VideoPage'
 import '../styles/App.css'
 
 function App() {
 	return (
-		<div className="App">
-			Hello World!
-		</div>
+		<Router>
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path="/watch" element={<VideoPage />} />
+			</Routes>
+		</Router>
 	)
 }
 
