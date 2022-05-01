64bc2ba441acc122964d6efecd191a600e007a58
Deekshant Yadav <d33kshant@gmail.com>
2022-05-01 11:59:22 +0530

    Serve react build

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+require('dotenv').config()
 const express = require('express')
+const path = require('path')
+const fileupload = require('express-fileupload')
 
-const { disconnect } = require('./database')
+const pool = require('./database')
 const videoRoute = require('./routes/video.route')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(express.json())
+app.use(fileupload({ createParentPath: true }))
 
-app.use('/', express.static('public'))
+app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
-app.use('/video', videoRoute)
+app.use('/api/video', videoRoute)
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+})
 
 const server = app.listen(PORT, ()=>{
 	console.log('Server listening on port:', PORT)
@@ -19,7 +27,7 @@ const server = app.listen(PORT, ()=>{
 const gracefulShutdown = signal => {
 	process.on(signal, async () => {
 		server.close()
-		await disconnect()
+		await pool.end()
 		console.log('Server Closed:', signal)
 		process.exit(0)
 	})
