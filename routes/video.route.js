1595403e2ae92026f728d1df523b562cbb68c960
Deekshant Yadav <d33kshant@gmail.com>
2022-05-01 11:58:36 +0530

    Add new route for view and videos

--- routes/video.route.js
+++ routes/video.route.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
-const { streamVideo, uploadVideo } = require('../controllers/video.controller')
+const { streamVideo, uploadVideo, getVideos, updateViews } = require('../controllers/video.controller')
 
-router.get('/', streamVideo)
+router.get('/', getVideos)
 router.post('/', uploadVideo)
+router.get('/stream', streamVideo)
+router.get('/view/:id', updateViews)
 
 module.exports = router
